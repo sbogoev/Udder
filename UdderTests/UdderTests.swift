e44d083f3e3238efb5fcdb3e5717daa2f81c3ea7
Stoyan Bogoev <sbogoev@yahoo.com>
2024-12-13 18:52:03 +0200

    Initial Commit

---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Stoyan Bogoev on 13.12.24.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
